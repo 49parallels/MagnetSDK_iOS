106883781463a1a3e5d96b8d280141319f7b204b
Eternal Vigilance <neutrumz@gmail.com>
2019-11-09 18:39:16 +0100

    Initial Commit

---
+++ MagnetSDK/MagnetSDK.h
@@ -0,0 +1,19 @@
+//
+//  MagnetSDK.h
+//  MagnetSDK
+//
+//  Created by e.d.neutrum on 09/11/2019.
+//  Copyright © 2019 e.d.neutrum. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MagnetSDK.
+FOUNDATION_EXPORT double MagnetSDKVersionNumber;
+
+//! Project version string for MagnetSDK.
+FOUNDATION_EXPORT const unsigned char MagnetSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MagnetSDK/PublicHeader.h>
+
+
